2183738890dd1aafa4841b54fe2b4a8a6bff74d5
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-10-03 23:57:39 -0300

    adding products card

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import './bootstrap.min.css'
+import Header from './components/nav/Header'
+import Footer from './components/nav/Footer'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>
-
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Home />
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
